a7116c3735a4e9f48465e621d5b79cfad1c13121
warisqr007 <ec2-user@ip-172-31-91-56.ec2.internal>
2022-02-27 07:52:24 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,18 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.order(params[:sort]) 
+    @sort_column = params[:sort]
+    if params[:sort] == "title"
+      @text_class_title = "text-success"
+    else
+      @text_class = "text-primary"
+    end
+    if params[:sort] == "release_date"
+      @text_class_date = "text-success"
+    else
+      @text_class = "text-primary"
+    end
   end
 
   def new
